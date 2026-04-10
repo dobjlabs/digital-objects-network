dbe7f6e55f3aa5c9c0b51baa4877a947f1f46c9e
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2026-04-10 12:58:49 -0700

    update test

--- craft_sdk/src/lib.rs
+++ craft_sdk/src/lib.rs
@@ -1100,7 +1100,6 @@ mod tests {
     use std::sync::Arc;
 
     use common::test_state::TestState;
-    use hex::FromHex;
     use lt_eq_u256_pod::LtEqU256Pod;
     use pod2::{
         frontend::{MainPod, Operation},
@@ -1382,17 +1381,11 @@ mod tests {
         let dependencies = vec![
             Dependency::Intro {
                 pred: "Vdf(count, input, output)",
-                hash: Hash::from_hex(
-                    "b77a964de74c8569e6c6172692bb50147df9334fd9b572abc8d4d9c688a40e06",
-                )
-                .unwrap(),
+                hash: *vdfpod::STANDARD_VDF_VD_HASH,
             },
             Dependency::Intro {
                 pred: "LtEqU256(lhs, rhs)",
-                hash: Hash::from_hex(
-                    "2e79114ee823f4783ab5b6eb93b49abba87fb69b4d14de4cf1d78648ade73529",
-                )
-                .unwrap(),
+                hash: *lt_eq_u256_pod::STANDARD_LT_EQ_U256_VD_HASH,
             },
         ];
 
