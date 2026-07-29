d5d43cbdec5f6904bc89438343b36fb63288b7d4
Dhole <dhole@riseup.net>
2026-07-29 16:58:04 +0200

    update pod2 contianer DB impl

--- services/synchronizer/src/app_db.rs
+++ services/synchronizer/src/app_db.rs
@@ -4,12 +4,12 @@ use anyhow::{anyhow, Context, Result};
 use pod2::{
     backends::plonky2::primitives::merkletree::{self, MerkleProof},
     middleware::{
-        containers::{Array, Set},
-        db::DB as PodDb,
+        containers::{Array, ContainerKind, Set},
+        db::{Read, DB as PodDb, TX as PodTx},
         Hash, RawValue, Value, EMPTY_HASH,
     },
 };
-use rocksdb::{Options, TransactionDB, TransactionDBOptions};
+use rocksdb::{DBAccess, Options, ReadOptions, Transaction, TransactionDB, TransactionDBOptions};
 
 use crate::head::StateRoots;
 
@@ -24,6 +24,13 @@ fn value_key(raw: RawValue) -> Vec<u8> {
     let mut k = Vec::with_capacity(34);
     k.extend_from_slice(b"v/");
     k.extend_from_slice(&raw.to_bytes());
+    k
+}
+
+fn kind_key(root: Hash) -> Vec<u8> {
+    let mut k = Vec::with_capacity(2 + 4);
+    k.extend_from_slice(b"k/");
+    k.extend_from_slice(&RawValue(root.0).to_bytes());
     k
 }
 
@@ -139,28 +146,131 @@ impl AppDb {
     }
 }
 
-impl merkletree::db::DB for AppDb {
-    fn load_node(&self, hash: Hash) -> Result<Option<merkletree::Node>> {
-        if hash == EMPTY_HASH {
-            return Ok(Some(merkletree::Node::Intermediate(
-                merkletree::Intermediate::new(EMPTY_HASH, EMPTY_HASH),
-            )));
-        }
-
-        match self.db.get(node_key(hash))? {
-            None => Ok(None),
-            Some(bytes) => Ok(Some(merkletree::Node::decode(bytes.as_ref())?)),
-        }
+fn load_node_db(db: &impl DBAccess, hash: Hash) -> Result<Option<merkletree::Node>> {
+    if hash == EMPTY_HASH {
+        return Ok(Some(merkletree::Node::Intermediate(
+            merkletree::Intermediate::new(EMPTY_HASH, EMPTY_HASH),
+        )));
     }
 
-    fn store_node(&mut self, node: merkletree::Node) -> Result<()> {
-        self.db
-            .put(node_key(node.hash()), node.encode()?)
-            .map_err(|err| anyhow!("rocksdb transaction put failed: {err}"))
+    let node_key = node_key(hash);
+    match db
+        .get_opt(&node_key, &ReadOptions::default())
+        .map_err(|e| anyhow!("rocksdb: get failed: {e}"))?
+    {
+        None => Ok(None),
+        Some(bytes) => Ok(Some(merkletree::Node::decode(bytes.as_ref())?)),
     }
 }
 
-impl PodDb for AppDb {
+fn store_node_tx<'a>(tx: &Transaction<'a, TransactionDB>, node: merkletree::Node) -> Result<()> {
+    let node_key = node_key(node.hash());
+    tx.put(&node_key, node.encode()?)
+        .map_err(|e| anyhow!("rocksdb transaction put failed: {e}"))
+}
+
+impl merkletree::db::Read for AppDb {
+    fn load_node(&self, hash: Hash) -> Result<Option<merkletree::Node>> {
+        load_node_db(&*self.db, hash)
+    }
+}
+
+impl merkletree::db::DB for AppDb {
+    fn tx<'a>(&'a self) -> Box<dyn merkletree::db::TX + 'a> {
+        PodDb::tx(self)
+    }
+}
+
+pub(crate) struct AppTx<'a> {
+    tx: rocksdb::Transaction<'a, rocksdb::TransactionDB>,
+    db: AppDb,
+}
+
+impl<'a> merkletree::db::Read for AppTx<'a> {
+    fn load_node(&self, hash: Hash) -> anyhow::Result<Option<merkletree::Node>> {
+        load_node_db(&self.tx, hash)
+    }
+}
+
+impl<'a> merkletree::db::TX for AppTx<'a> {
+    fn store_node(&mut self, node: merkletree::Node) -> anyhow::Result<()> {
+        store_node_tx(&self.tx, node)
+    }
+    fn commit(self: Box<Self>) -> anyhow::Result<()> {
+        panic!("use middleware::db::TX::commit")
+    }
+}
+
+impl<'a> Read for AppTx<'a> {
+    fn load_value(&self, raw: RawValue) -> anyhow::Result<Option<Value>> {
+        match self.tx.get(value_key(raw))? {
+            None => Ok(None),
+            Some(bytes) => Ok(Some({
+                if bytes.is_empty() {
+                    Value::from(raw)
+                } else {
+                    Value::from_bytes(bytes.as_ref(), self.db.clone_box())?
+                }
+            })),
+        }
+    }
+    fn load_kind(&self, root: Hash) -> anyhow::Result<Option<ContainerKind>> {
+        if root == EMPTY_HASH {
+            return Ok(Some(
+                *ContainerKind::default()
+                    .set_dictionary()
+                    .set_set()
+                    .set_array(),
+            ));
+        }
+        // We use `get_for_update` because this method is part of a transaction, and it will be
+        // used by `update_kind`, so we want and exclusive lock after the value is read to
+        // guarantee no data-races in the merge update.
+        self.tx
+            .get_for_update(kind_key(root), true)
+            .map(|opt| {
+                opt.map(|bytes| match bytes.len() {
+                    1 => Ok(ContainerKind(bytes[0])),
+                    l => Err(anyhow!("db: invalid kind len: {}", l)),
+                })
+            })?
+            .transpose()
+    }
+}
+
+impl<'a> PodTx for AppTx<'a> {
+    fn store_value(&mut self, value: Value) -> anyhow::Result<()> {
+        let value_key = value_key(value.raw());
+        if let Some(old_value_bytes) = self.tx.get(&value_key)? {
+            // Never overwrite an old value with a RawValue.  Skip overwrite if old value is
+            // already non-RawValue.
+            if value.is_raw() || !old_value_bytes.is_empty() {
+                return Ok(());
+            }
+        };
+        let value_bytes = if value.is_raw() {
+            // For RawValue we store an empty vector because it's a duplicate of the key.
+            // This way we can easily check for RawValue without decoding.
+            vec![]
+        } else {
+            Value::to_bytes(&value)
+        };
+        Ok(self.tx.put(value_key, value_bytes)?)
+    }
+    fn update_kind(&mut self, root: Hash, kind: ContainerKind) -> anyhow::Result<()> {
+        let kind = match self.load_kind(root).expect("ok") {
+            Some(old_kind) => ContainerKind(old_kind.0 | kind.0),
+            None => kind,
+        };
+        let kind_key = kind_key(root);
+        Ok(self.tx.put(&kind_key, [kind.0])?)
+    }
+    fn commit(self: Box<Self>) -> anyhow::Result<()> {
+        Ok(self.tx.commit()?)
+    }
+}
+
+impl Read for AppDb {
     fn load_value(&self, raw: RawValue) -> anyhow::Result<Option<Value>> {
         match self.db.get(value_key(raw))? {
             None => Ok(None),
@@ -173,33 +283,31 @@ impl PodDb for AppDb {
             })),
         }
     }
-
-    fn store_value(&mut self, value: Value) -> anyhow::Result<()> {
-        let value_key = value_key(value.raw());
-        let tx = self.db.transaction();
-        if let Some(old_value_bytes) = tx.get_for_update(&value_key, true)? {
-            let is_raw = old_value_bytes.is_empty();
-            // If we had a non-RawValue stored don't overwrite it (specially not with a
-            // RawValue).   Also skip redundant RawValue overwrite.
-            if !is_raw || value.is_raw() {
-                return Ok(());
-            }
+    fn load_kind(&self, root: Hash) -> anyhow::Result<Option<ContainerKind>> {
+        if root == EMPTY_HASH {
+            return Ok(Some(
+                *ContainerKind::default()
+                    .set_dictionary()
+                    .set_set()
+                    .set_array(),
+            ));
         }
-        let value_bytes = if value.is_raw() {
-            // For RawValue we store an empty vector because it's a duplicate of the key.
-            // This way we can easily check for RawValue without decoding.
-            vec![]
-        } else {
-            Value::to_bytes(&value)
-        };
-        tx.put(value_key, value_bytes)?;
-        Ok(tx.commit()?)
+        Ok(self.db.get(kind_key(root)).map(|opt| {
+            opt.map(|bytes| {
+                assert_eq!(1, bytes.len());
+                ContainerKind(bytes[0])
+            })
+        })?)
     }
+}
 
-    fn is_persistent(&self) -> bool {
-        true
+impl PodDb for AppDb {
+    fn tx<'a>(&'a self) -> Box<dyn PodTx + 'a> {
+        Box::new(AppTx {
+            tx: self.db.transaction(),
+            db: self.clone(),
+        })
     }
-
     fn clone_box(&self) -> Box<dyn PodDb> {
         Box::new(self.clone())
     }
