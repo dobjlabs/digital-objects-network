eb95bf0b733efa9b9be0a5d4affcbedb4ef6e7de
Eduard S. <eduardsanou@posteo.net>
2026-08-04 17:55:43 +0200

    sdk: intro pods error handling (#210)
    * handle intro pods errors
    
    * display anyhow errors from rhai eval
    
    * cargo clippy and fmt

--- libs/sdk/src/error.rs
+++ libs/sdk/src/error.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use rhai::{EvalAltResult, Position};
 
 #[derive(Debug, thiserror::Error)]
@@ -14,10 +16,22 @@ impl From<anyhow::Error> for SdkError {
     }
 }
 
+fn innermost(e: &EvalAltResult) -> &EvalAltResult {
+    match e {
+        EvalAltResult::ErrorInFunctionCall(_, _, inner, _) => innermost(inner),
+        other => other,
+    }
+}
+
 impl From<Box<EvalAltResult>> for SdkError {
     fn from(e: Box<EvalAltResult>) -> Self {
-        let position = e.position();
-        let msg = format!("{e}");
-        Self::Eval(msg, position)
+        // Unwrap the runtime error because otherwise the Display impl only formats the type and
+        // not the value.
+        if let EvalAltResult::ErrorRuntime(payload, pos) = innermost(&e)
+            && let Some(e) = payload.read_lock::<Rc<anyhow::Error>>()
+        {
+            return Self::Eval(format!("{}", &*e), *pos);
+        }
+        Self::Eval(format!("{e}"), e.position())
     }
 }
